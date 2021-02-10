b3e92a691ea5c750cf58b92180731fb26846e736
WernerWildenboer <wwildenboer@gmail.com>
2021-02-10 12:09:12 +0200

    Hover text added

--- reactrouter/src/Components/Navbar.js
+++ reactrouter/src/Components/Navbar.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import { Link, NavLink, withRouter } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
+import Tooltip from "@material-ui/core/Tooltip";
 
 const Navbar = () => {
-    return (
-        <nav className="nav-wrapper red darken-3">
-            <div className="container navbar">
-               <Link className="brand-logo" to="/">Poke' Times</Link> 
-                <ul className="right">
-                    <li><NavLink to="/" >Home</NavLink></li>           
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container navbar">
+        <Link className="brand-logo" to="/">
+          Poke' Times
+        </Link>
+        <ul className="right">
+          <Tooltip title="Takes you Home">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+          </Tooltip>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default withRouter(Navbar)
+export default withRouter(Navbar);
